d5773562c45edf5065780a356fa45fa56ab0018e
Kai Wohlfahrt <kai@charmtx.com>
2024-11-23 17:40:03 +0100

    Handle nil in configmap data

--- internal/provider/crd/fixtures/core/fn.tf
+++ internal/provider/crd/fixtures/core/fn.tf
@@ -11,10 +11,16 @@ output "pod" {
   value = provider::k8scrd::parse_pod_v1({
     apiVersion = "v1"
     kind       = "Pod"
-    metadata = {
-      name      = "bar"
-      namespace = "default"
-    }
-    spec = { containers = [{ name : "ubuntu", image : "ubuntu:22.04" }] }
+    metadata   = { name = "bar", namespace = "default" }
+    spec       = { containers = [{ name : "ubuntu", image : "ubuntu:22.04" }] }
+  })
+}
+
+output "configmap" {
+  value = provider::k8scrd::parse_configmap_v1({
+    apiVersion = "v1"
+    kind       = "ConfigMap"
+    metadata   = { name = "bar", namespace = "default" }
+    data       = null // Sometimes seen in manifests with empty data
   })
 }
