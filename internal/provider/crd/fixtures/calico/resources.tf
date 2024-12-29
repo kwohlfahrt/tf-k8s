1172738850b3130d14767ebe3696164d220231e5
Kai Wohlfahrt <kai@charmtx.com>
2024-12-29 12:46:44 +0100

    Set up calico apiserver

--- internal/provider/crd/fixtures/calico/resources.tf
+++ internal/provider/crd/fixtures/calico/resources.tf
@@ -13,3 +13,10 @@ resource "k8scrd_ippool_crd_projectcalico_org_v1" "bar" {
     spec     = { cidr = "198.51.100.8/30" }
   }
 }
+
+resource "k8scrd_ippool_projectcalico_org_v3" "bar" {
+  manifest = {
+    metadata = { name = "qux" }
+    spec     = { cidr = "192.0.2.0/24" }
+  }
+}
